60d1f270ec670a08d7b0f62348ab80ee30a745b5
GIAO007 <253996348@qq.com>
2021-02-02 21:13:15 +0800

    第二次提交

--- js/header.js
+++ js/header.js
@@ -1,26 +1,29 @@
-var lo_li5 = document.querySelector('.lo_ul .lo_li5')
-var lo_li6 = document.querySelector('.lo_ul .lo_li6')
-var lo_li7 = document.querySelector('.lo_ul .lo_li7')
-
+var lo_ul = document.querySelector('.lo_ul')
+var lo_li = document.querySelectorAll('.lo_li')
+var li5 = document.querySelector('#li5')
+var li6 = document.querySelector('#li6')
+var li7 = document.querySelector('#li7')
 var lo_ol1 = document.querySelector('.lo_ol1')
 var lo_ol2 = document.querySelector('.lo_ol2')
 var down = document.querySelector('.down')
 
-lo_li5.onmouseover = function (){
+on(lo_ul,'mouseover','#li5',function (){
     lo_ol1.style.display = 'block'
-}
-lo_li5.onmouseout = function (){
-    lo_ol1.style.display = 'none'
-}
-lo_li6.onmouseover = function (){
+    li5.onmouseleave = function (){
+        lo_ol1.style.display = 'none'
+    }
+})
+
+on(lo_ul,'mouseover','#li6',function (){
     lo_ol2.style.display = 'block'
-}
-lo_li6.onmouseout = function (){
-    lo_ol2.style.display = 'none'
-}
-lo_li7.onmouseover = function (){
+    li6.onmouseleave = function (){
+        lo_ol2.style.display = 'none'
+    }
+})
+
+on(lo_ul,'mouseover','#li7',function (){
     down.style.display = 'block'
-}
-lo_li7.onmouseout = function (){
-    down.style.display = 'none'
-}
+    li7.onmouseleave = function (){
+        down.style.display = 'none'
+    }
+})
