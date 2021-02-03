8f945723c5a8b4b39316465af0063f769a1e9104
GIAO007 <253996348@qq.com>
2021-02-03 22:08:45 +0800

    第四次提交

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,20 @@ let load = require('gulp-load-plugins')()
 let del = require('del')
 
 task('delDist',async ()=>{
-    await del('./dist')
+    await del('./gulp_cli/dist')
 })
 
 // html处理
 task('html',async ()=>{
-    src(['./*.html','./*.json','./*.md','./*.js'])
-    .pipe(dest('./dist'))
+    src('./*.html')
+    .pipe(dest('./gulp_cli/dist'))
     .pipe(load.connect.reload())
 })
 
 // css处理
 task('css',async ()=>{
     src('./css/*.css')
-    .pipe(dest('./dist/css'))
+    .pipe(dest('./gulp_cli/dist/css'))
     .pipe(load.connect.reload())
 })
 
@@ -24,28 +24,28 @@ task('css',async ()=>{
 //js处理
 task('js',async ()=>{
     src('./js/*.js')
-    .pipe(dest('./dist/js'))
+    .pipe(dest('./gulp_cli/dist/js'))
     .pipe(load.connect.reload())
 })
 
 //json处理
 task('data',async ()=>{
     src('./data/*.json')
-    .pipe(dest('./dist/data'))
+    .pipe(dest('./gulp_cli/dist/data'))
     .pipe(load.connect.reload())
 })
 
 //img处理
 task('img',async ()=>{
     src('./img/*.*')
-    .pipe(dest('./dist/img'))
+    .pipe(dest('./gulp_cli/dist/img'))
     .pipe(load.connect.reload())
 })
 
 // 启动一个服务，实现自动刷新
 task('reload',async ()=>{
     load.connect.server({
-      root: './dist',//设置根目录
+      root: './gulp_cli/dist',//设置根目录
       livereload: true//开启自动刷新
     })
 })
